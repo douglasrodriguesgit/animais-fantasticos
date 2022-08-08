ba921e08601361f9b9ba5efc0119b7794b4364c9
Douglas Rodrigues <douglasrodrigues.git@gmail.com>
2022-08-08 19:16:17 -0300

    Adicionado tooltip no mapa referente ao endereço

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,24 +1,23 @@
-export function initModal() {
-  const botaoAbrir = document.querySelector("[data-modal='abrir']");
-  const botaoFechar = document.querySelector("[data-modal='fechar']");
-  const containerModal = document.querySelector("[data-modal='container']");
+export function initModal() {}
 
-  if (botaoAbrir && botaoFechar && containerModal) {
-    function toggleModal(event) {
-      event.preventDefault();
-      containerModal.classList.toggle("ativo");
-      console.log(event);    }
+const botaoAbrir = document.querySelector("[data-modal='abrir']");
+const botaoFechar = document.querySelector("[data-modal='fechar']");
+const containerModal = document.querySelector("[data-modal='container']");
 
-
-    function cliqueForaModal(event) {
-      event.preventDefault();
-      if (event.target === this) {
-        toggleModal(event);
-      }
-    }
-
-    botaoAbrir.addEventListener("click", toggleModal);
-    botaoFechar.addEventListener("click", toggleModal);
-    containerModal.addEventListener("click", cliqueForaModal);
+if (botaoAbrir && botaoFechar && containerModal) {
+  function toggleModal(event) {
+    event.preventDefault();
+    containerModal.classList.toggle("ativo");
   }
+
+  function cliqueForaModal(event) {
+    event.preventDefault();
+    if (event.target === this) {
+      toggleModal(event);
+    }
+  }
+
+  botaoAbrir.addEventListener("click", toggleModal);
+  botaoFechar.addEventListener("click", toggleModal);
+  containerModal.addEventListener("click", cliqueForaModal);
 }
