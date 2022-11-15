23d4e29962de70781734b93b8e57256e41f61425
Douglas Rodrigues <douglasrodrigues.git@gmail.com>
2022-11-15 12:33:19 -0300

    acerto retorno de classes

--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -1,32 +1,34 @@
 export default class TabNav {
-  constructor(menu, content){
+  constructor(menu, content) {
     this.tabMenu = document.querySelectorAll(menu);
     this.tabContent = document.querySelectorAll(content);
-    this.activeClass= 'ativo'
+    this.activeClass = "ativo";
   }
 
   // ativa a tab de acordo com o index da mesma
   activeTab(index) {
-  this.tabContent.forEach((section) => {
+    this.tabContent.forEach((section) => {
       section.classList.remove(this.activeClass);
     });
-    const direcao =this.tabContent[index].dataset.anime;
+    const direcao = this.tabContent[index].dataset.anime;
     this.tabContent[index].classList.add("ativo", direcao);
   }
 
   // adiciona os eventos nas tabs
-  addTabNavEvent(){
-   this.tabMenu.forEach((itemMenu, index) => {
+  addTabNavEvent() {
+    this.tabMenu.forEach((itemMenu, index) => {
       itemMenu.addEventListener("click", () => {
         this.activeTab(index);
       });
     });
   }
 
-  init(){
-   if (this.tabMenu.length && this.tabContent.length){    
-    // ativar primeiro item
-    this.activeTab(0)
-    this.addTabNavEvent()                          
-   }
-  }}
+  init() {
+    if (this.tabMenu.length && this.tabContent.length) {
+      // ativar primeiro item
+      this.activeTab(0);
+      this.addTabNavEvent();
+    }
+    return this;
+  }
+}
